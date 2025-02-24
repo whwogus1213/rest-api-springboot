bd5271dc884ddd5d196a43f2d54c4f73589ff436
90000219 <jae_hyun.cho@kt.com>
2025-02-24 15:10:35 +0900

    Event Entity

---
+++ src/test/java/com/jaehyun/restapispringboot/events/EventTest.java
@@ -0,0 +1,28 @@
+package com.jaehyun.restapispringboot.events;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventTest {
+
+  @Test
+  public void builder() {
+    Event event = Event.builder().build();
+    assertThat(event).isNotNull();
+
+  }
+
+  @Test
+  public void javaBean() {
+    String name = "Event";
+    String description = "Spring";
+
+    Event event = new Event();
+    event.setName(name);
+    event.setDescription(description);
+
+    assertThat(event.getName()).isEqualTo(name);
+    assertThat(event.getDescription()).isEqualTo(description);
+  }
+}
