7d2d4fbb6222cc3153b31071bf3237b93e57ca01
90000219 <jae_hyun.cho@kt.com>
2025-02-24 14:10:07 +0900

    init

---
+++ src/main/java/com/jaehyun/restapispringboot/RestApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jaehyun.restapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaehyun/restapispringboot/RestApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaehyun.restapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
