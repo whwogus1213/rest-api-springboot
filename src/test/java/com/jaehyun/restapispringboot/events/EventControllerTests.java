5aaed73d24d022810c337d53ebdf17dc679ca060
90000219 <jae_hyun.cho@kt.com>
2025-03-05 15:45:41 +0900

    Add test description

--- src/test/java/com/jaehyun/restapispringboot/events/EventControllerTests.java
+++ src/test/java/com/jaehyun/restapispringboot/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package com.jaehyun.restapispringboot.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jaehyun.restapispringboot.common.TestDescription;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -34,6 +35,7 @@ public class EventControllerTests {
 
 
   @Test
+  @TestDescription("정상적으로 이벤트를 생성하는 테스트")
   public void createEvent() throws Exception {
     EventDto event = EventDto.builder()
             .name("Spring")
@@ -61,6 +63,7 @@ public class EventControllerTests {
   }
 
   @Test
+  @TestDescription("입력 받을 수 없는 값을 사용한 경우에 에러가 발생하는 테스트")
   public void createEvent_Bad_Request() throws Exception {
     Event event = Event.builder()
             .id(100)
@@ -89,6 +92,7 @@ public class EventControllerTests {
   }
 
   @Test
+  @TestDescription("입력값이 비어있는 경우에 에러가 발생하는 테스트")
   public void createEvent_Bad_request_Empty_input() throws Exception {
     EventDto eventDto = EventDto.builder().build();
 
@@ -100,6 +104,7 @@ public class EventControllerTests {
   }
 
   @Test
+  @TestDescription("입력값이 잘못된 경우에 에러가 발생하는 테스트")
   public void createEvent_Bad_request_Wrong_input() throws Exception {
     EventDto eventDto = EventDto.builder()
             .name("Spring")
